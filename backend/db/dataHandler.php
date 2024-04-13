2d6f7133efdf0016bf5c2ccb44010b971d4d96ed
Filip Lazarevic <filip.lazarevic984@gmail.com>
2024-04-14 00:49:07 +0200

    Änderungen übernommen

--- backend/db/dataHandler.php
+++ backend/db/dataHandler.php
@@ -2,13 +2,15 @@
 include("./models/appointment.php");
 class DataHandler
 {
-    private static function getDemoData()
+    
+
+    public static function getDemoData()
     {
         $demodata = [
-            new Appoitment("Webscript","Wien","March 23 17:00","March 23 17:15"),
-            new Appoitment("Mathe","Wien","March 23 17:15","March 23 17:30"),
-            new Appoitment("Deutsch","München","March 23 17:30","March 23 17:45"),
-            new Appoitment("English","Düsseldorf","March 23 17:45","March 23 18:00"),
+            new Appoitment("Webscript", "Wien", "March 23 17:00", "March 23 17:15"),
+            new Appoitment("Mathe", "Wien", "March 23 17:15", "March 23 17:30"),
+            new Appoitment("Deutsch", "München", "March 23 17:30", "March 23 17:45"),
+            new Appoitment("English", "Düsseldorf", "March 23 17:45", "March 23 18:00"),
         ];
         return $demodata;
     }
