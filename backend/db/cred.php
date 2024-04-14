f81ca02d210d2e331e8a2775adde9cb1a71eaca5
Filip Lazarevic <filip.lazarevic984@gmail.com>
2024-04-14 04:05:25 +0200

    Hab versucht DB zu verbinden(nicht geschafft)

---
+++ backend/db/cred.php
@@ -0,0 +1,5 @@
+<?php
+$_ENV['DB_NAME'] = 'appointments_finder';
+$_ENV['DB_USER'] = 'appointments_user';
+$_ENV['DB_PASSWORD'] = 'AppointmentsUser';
+$_ENV['DB_HOST'] = 'localhost';
--- backend/db/db.php
+++ backend/db/db.php
@@ -1,13 +1,18 @@
 <?php
-    $servername = "localhost";
-    $username = "bif2webscriptinguser";
-    $password = "bif2023";
-    $dbname = "Appointments_Finder";
-    // Verbindung erstellen
-    $db = new mysqli($servername,$username,$password,$dbname);
-    // Verbindungsfehlermeldung
-    if($db->connect_error){
-        echo "Error connecting to $dbname: " .  $db->connect_error;
+// Login database credentials from cred.php
+require_once 'cred.php';
+ob_start();
+
+// Establish a mysqli database connection. If connection fails, exit with status 1; Otherwise, define a constant 'db'
+try {
+    $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
+    if ($db->connect_error) {
         exit(1);
     }
-?>
+    //Konstante
+    define('db', $db);
+} catch (Exception $e) {
+    // Display a message if an exception occurs during database connection
+    echo "Website is currently not available!";
+    exit;
+}
