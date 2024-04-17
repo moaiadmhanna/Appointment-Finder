7e77efe1e55306f6ab17dac1c8635fcccabadf52
muayad <moaiadmhana999@gmail.comm>
2024-04-17 18:51:43 +0200

    Design fertig

--- backend/db/dataHandler.php
+++ backend/db/dataHandler.php
@@ -5,12 +5,25 @@ class DataHandler
 {
     
 
-    public static function getAllApointments()
+    public static function getApointments($param = null)
     {
         global $db;
         $resultArray = array();
-        $sql = "SELECT * FROM Appointments";
-        $result=$db->query($sql);
+        if($param == null)
+        {
+            $sql = "SELECT * FROM Appointments
+            ORDER BY Voting , Date desc";
+            $result=$db->query($sql);
+            while($row = $result->fetch_assoc()){
+                array_push($resultArray,$row);
+            }
+            return $resultArray;
+        }
+        $sql = "SELECT * FROM Appointments WHERE Title LIKE CONCAT('%', ?, '%') ORDER BY Voting , Date desc";
+        $stmt = $db->prepare($sql);
+        $stmt->bind_param("s",$param);
+        $stmt->execute();
+        $result = $stmt->get_result();
         while($row = $result->fetch_assoc()){
             array_push($resultArray,$row);
         }
